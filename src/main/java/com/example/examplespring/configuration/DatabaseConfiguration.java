c998cd463952a9427121a08a6f0d648a6280b4fe
study-cho <dmcho.ex@gmail.com>
2022-07-16 16:28:35 +0900

    DataSource Bean 등록

---
+++ src/main/java/com/example/examplespring/configuration/DatabaseConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.examplespring.configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
