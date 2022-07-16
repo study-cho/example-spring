3b35f660a6d764ca58c7f34630a1f6fe73216788
study-cho <dmcho.ex@gmail.com>
2022-07-16 16:34:12 +0900

    도메인 클래스 생성

---
+++ src/main/java/com/example/examplespring/mvc/domain/Board.java
@@ -0,0 +1,15 @@
+package com.example.examplespring.mvc.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Board {
+
+    private int boardSeq;
+    private String title;
+    private String contents;
+    private Date regDate;
+
+}
