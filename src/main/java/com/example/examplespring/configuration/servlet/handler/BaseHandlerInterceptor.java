87c6f99f347d0cd30a486ce7ea954c205f201bf0
study-cho <dmcho.ex@gmail.com>
2022-07-18 16:08:41 +0900

    interceptor 클래스 추가

---
+++ src/main/java/com/example/examplespring/configuration/servlet/handler/BaseHandlerInterceptor.java
@@ -0,0 +1,25 @@
+package com.example.examplespring.configuration.servlet.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BaseHandlerInterceptor implements HandlerInterceptor {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("preHandle requestURI : {}", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("postHandle requestURI : {}", request.getRequestURI());
+    }
+}
