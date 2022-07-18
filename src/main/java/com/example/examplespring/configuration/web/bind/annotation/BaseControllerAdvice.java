6fad12aa6ce09a893abc35b60139ebb67fa99a7a
study-cho <dmcho.ex@gmail.com>
2022-07-18 14:25:33 +0900

    BaseControllerAdvice 작성 및 일부 수정

---
+++ src/main/java/com/example/examplespring/configuration/web/bind/annotation/BaseControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.example.examplespring.configuration.web.bind.annotation;
+
+import com.example.examplespring.configuration.exception.BaseException;
+import com.example.examplespring.configuration.http.BaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class BaseControllerAdvice {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @ExceptionHandler(value = { BaseException.class })
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    private BaseResponse<?> handleBaseException(BaseException e, WebRequest request) {
+        return new BaseResponse<String>(e.getResponseCode(), messageSource.getMessage(e.getResponseCode().name(), e.getArgs(), null));
+    }
+}
